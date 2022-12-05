1f9dd875d3bcf08dc19f24ee80e6f68441836135
Evening <evening@admins-iMac.local>
2022-12-05 17:48:05 +0800

    12.05 hicheel

---
+++ Js/ArrayOfNumbers.js
@@ -0,0 +1,58 @@
+// let studentNames = ['boldo', 'dorjo', 'tsetsge','dulmaa']
+// let i = 0;
+// while (i < studentNames.length) {
+// if (studentNames[i][0] === 'd') {
+// console.log(studentNames[i]);
+// }
+// i++;
+// }
+
+// let studentAges = [10, 20, 15, 19, 18,19 ,19];
+// let i = 0;
+// let sum = 0;
+// while (i < studentAges.length) {
+// sum+=studentAges[i]
+// i++;
+// }
+// let avarage = sum/studentAges.length
+// console.log(avarage);
+
+// 1. arrayOfNumbers гэсэн variable зарлаад дараах тоонуудыг оруул. 43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11
+//  
+// 2. Array доторх тоонуудын нийлбэрийг ол.
+// let sum = 0;
+
+// for(let i = 0; i < arrayOfNumbers.length; i++){
+//     sum+=arrayOfNumbers[i];
+// } 
+// console.log(sum)
+
+// 3. Хамгийн их тоог ол.
+// let max = 0;
+
+// for(let i=0; i < arrayOfNumbers[i]; i++){
+//     if(max < arrayOfNumbers[i]) {
+//         max = arrayOfNumbers[i];
+//     }
+// }   
+// console.log(`max utga ni :${max}`);
+
+// 4. Хамгийн бага тоог ол.
+// let min = arrayOfNumbers[0];
+
+// for(let i = 0; i < arrayOfNumbers[i]; i++){
+//     if(min > arrayOfNumbers[i]) {
+//         min = arrayOfNumbers[i];
+//     }
+// }
+// console.log(`min utga ni:${min}`)
+
+// 5. Array - ийн эхэнд дурын 1 тоог нэм.
+
+// arrayOfNumbers.unshift(34);
+// console.log (arrayOfNumbers)
+
+// 6. Array - ийн төгсгөлд дурын 1 тоог нэм.
+
+// arrayOfNumbers.push(99);
+// console.log(arrayOfNumbers);
---
+++ Js/SimpleArray1.js
@@ -0,0 +1,15 @@
+// Array - ын хувьсагчийг itCompanies гэж зарлаж, Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг анхны утгыг оруулна уу. 
+let itCompanies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+
+// 1. console.log() ашиглан array хэвлэх
+// console.log(itCompanies)
+
+// 2.Array дахь компаниудын length хэвлэх
+// console.log(itCompanies.length)
+
+// 3.Эхний компани, дунд болон сүүлчийн компанийг хэвлэх
+// console.log(itCompanies[0]);
+// console.log(itCompanies[Math.floor(itCompanies.length/2)]);
+// console.log(itCompanies[itCompanies.length - 1]);
+
+// 4.Компани бүрийг хэвлэх
